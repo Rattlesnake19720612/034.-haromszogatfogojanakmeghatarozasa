21e12a0f186bad35db984839f9db6a074cfbde57
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-27 12:16:25 +0200

    △ átfogójának meghatározása

---
+++ 034. haromszogatfogojanakmeghatarozasa/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _034.haromszogatfogojanakmeghatarozasa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+
+            Console.WriteLine("Add meg a háromszög egyik befogójának értékét! ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Add meg a háromszög másik befogójának értékét! ");
+            b = double.Parse(Console.ReadLine());
+
+            c = Math.Sqrt(a * a + b * b);
+
+            c = Math.Round(c, 2);
+
+            Console.WriteLine("Az átfogó hossza:{0}", c);
+
+            Console.ReadKey();
+
+        }
+    }
+}
