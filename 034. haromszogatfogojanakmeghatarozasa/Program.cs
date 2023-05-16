02341003ee7a8073c6c466bb95320e6ce4138595
DGYKKSZKI <124397426+Rattlesnake19720612@users.noreply.github.com>
2023-05-16 22:34:00 +0200

    △ átfogójának meghatározása

--- 034. haromszogatfogojanakmeghatarozasa/Program.cs
+++ 034. haromszogatfogojanakmeghatarozasa/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,3 +28,17 @@ namespace _034.haromszogatfogojanakmeghatarozasa
         }
     }
 }
+
+/*Ez egy C# program, amely kiszámítja egy derékszögű háromszög átfogójának hosszát a megadott befogók alapján. A program bekéri a felhasználótól a háromszög egyik és másik befogójának hosszát, majd elvégzi a szükséges számításokat és kiírja az átfogó hosszát.
+Íme, hogyan működik a program lépésről lépésre:
+A program importálja a szükséges könyvtárakat.
+A program létrehozza a Main függvényt, amely a program belépési pontja.
+Deklarálja és inicializálja az a, b és c változókat a megfelelő típussal.
+A program kiírja a konzolra az üzenetet, hogy adja meg a háromszög egyik befogójának hosszát.
+Beolvassa a felhasználó által megadott értéket a Console.ReadLine metódussal, majd átalakítja double típusba a double.Parse metódus segítségével, és elmenti az a változóba.
+A program kiírja a konzolra az üzenetet, hogy adja meg a háromszög másik befogójának hosszát.
+Beolvassa a felhasználó által megadott értéket a Console.ReadLine metódussal, majd átalakítja double típusba a double.Parse metódus segítségével, és elmenti a b változóba.
+Kiszámítja az átfogó hosszát a Pitagorasz-tétel alapján: c = Math.Sqrt(a * a + b * b).
+Lehetőség szerint kerekíti az c változó értékét két tizedesjegyre a Math.Round metódussal.
+Kiírja az eredményt a konzolra a Console.WriteLine metódussal.
+A program várja, hogy a felhasználó nyomjon meg egy billentyűt a kilépéshez. Ezután a program kilép.*/
